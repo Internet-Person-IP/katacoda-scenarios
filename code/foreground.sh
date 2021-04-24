59f9a91a0167fa45609084d8c39319c266533725
Taqui <taqui@kth.se>
2021-04-25 00:58:25 +0200

    removed a command

--- code/foreground.sh
+++ code/foreground.sh
@@ -3,7 +3,6 @@
 
 echo "Waiting to complete Please Do not Click Ctrl-C it will cancel the background process and you will need to reload the tutorial"; while [ ! -f /opt/.backgroundfinished ]; do sleep 2; done; echo "Done"
 
-sudo apt-get install apt-transport-https ca-certificates gnupg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
