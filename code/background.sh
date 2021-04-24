0c0d7eda48e7ec0014f99124124ebc36b45b623c
Taqui <taqui@kth.se>
2021-04-25 00:15:33 +0200

    Fixed scripts to wait for clone and download and shit

--- code/background.sh
+++ code/background.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+echo "This is a background script with a long running process"
+
 ls -a
 git clone https://github.com/Internet-Person-IP/GKE-CI-CD-Tutorial
 cd GKE-CI-CD-Tutorial
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+
+echo "done" >> /opt/.backgroundfinished
+
