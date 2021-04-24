57312353035b00e0e65415782d9cb3e78a5ef3f9
Taqui <taqui@kth.se>
2021-04-25 01:00:04 +0200

    Added -y

--- code/foreground.sh
+++ code/foreground.sh
@@ -5,4 +5,4 @@ echo "Waiting to complete Please Do not Click Ctrl-C it will cancel the backgrou
 
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk
+sudo apt-get update && sudo apt-get install google-cloud-sdk -y
