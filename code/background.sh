83864a855c082e7f18728c9fa96b18a7b5f232df
Taqui <taqui@kth.se>
2021-04-25 00:11:17 +0200

    Trying to add terraform

--- code/background.sh
+++ code/background.sh
@@ -3,3 +3,6 @@
 ls -a
 git clone https://github.com/Internet-Person-IP/GKE-CI-CD-Tutorial
 cd GKE-CI-CD-Tutorial
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
