abe8c470b639b2c4b1c07af79bb5571c2699b6f9
Taqui <taqui@kth.se>
2021-04-25 00:19:23 +0200

    Fixing the foreground script

--- code/foreground.sh
+++ code/foreground.sh
@@ -1,8 +1,4 @@
 
 #!/bin/bash
 
-echo "Waiting to complete";
-while [ ! -f /opt/.backgroundfinished ];
-do sleep 2; 
-done; 
-echo "Done"
+echo "Waiting to complete Please Do not Click Ctrl-C it will cancel the background process and you will need to reload the tutorial"; while [ ! -f /opt/.backgroundfinished ]; do sleep 2; done; echo "Done"
