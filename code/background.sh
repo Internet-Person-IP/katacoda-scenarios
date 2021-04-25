fe9d75f2a6775affd4934c362469378cce289702
Taqui <taqui@kth.se>
2021-04-25 16:32:12 +0200

    Fixed Initial State

--- code/background.sh
+++ code/background.sh
@@ -4,7 +4,8 @@
 echo "This is a background script with a long running process"
 
 ls -a
-git clone https://github.com/Internet-Person-IP/GKE-CI-CD-Tutorial/tree/initialState
+git clone https://github.com/Internet-Person-IP/GKE-CI-CD-Tutorial
+git checkout initialState
 cd GKE-CI-CD-Tutorial
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
