fd7fb3d898c61733393c7fe57035ae3a09b2b432
Taqui <taqui@kth.se>
2021-04-25 16:29:54 +0200

    Added Initial Starting Values

--- code/background.sh
+++ code/background.sh
@@ -4,7 +4,7 @@
 echo "This is a background script with a long running process"
 
 ls -a
-git clone https://github.com/Internet-Person-IP/GKE-CI-CD-Tutorial
+git clone https://github.com/Internet-Person-IP/GKE-CI-CD-Tutorial/tree/initialState
 cd GKE-CI-CD-Tutorial
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
